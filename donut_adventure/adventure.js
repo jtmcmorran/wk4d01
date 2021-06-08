61bbd84dcb5afb99aabea1578922ad4716253870
James McMorran <jtmcmorran@gmail.com>
2021-06-08 16:38:55 -0400

    created our hero dougie

--- donut_adventure/adventure.js
+++ donut_adventure/adventure.js
@@ -1,1 +1,23 @@
-console.log("ayyy");
+class Hero{
+  constructor(name){
+    this.name = name;
+    this.health = 100;
+    this.weapons = {
+      sprinkleSpray: 5,
+      sugarShock: 10
+    }
+    this.catchPhrases = ['i\'m fresher than day old pizza',
+    'you can\'t count my calories']
+  }
+  talkSass(){
+       console.log(this.catchPhrases[Math.floor(Math.random()*this.catchPhrases.length)]);
+  }
+  announceHealth(){
+    console.log(this.health);
+  }
+  fight(){
+    console.log('i\'m ready to rumble');
+  }
+}
+
+let dougie = new Hero("dougie");
